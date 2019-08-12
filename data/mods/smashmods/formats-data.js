036e83220cff5893b885b73c4d6643deab4e8dad
DatSpookTho <nickguin123@gmail.com>
2019-08-11 21:21:03 -0400

    Smash Mods

---
+++ data/mods/smashmods/formats-data.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**@type {{[k: string]: ModdedTemplateFormatsData}} */
+let BattleFormatsData = {
+	steve: {
+		inherit: true,
+		tier: 'OU',
+	},
+};
+
+exports.BattleFormatsData = BattleFormatsData;
