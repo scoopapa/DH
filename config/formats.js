9a4a6e685ceb074389b6664d9c0eed39b7515a6d
scoopapa <pccompositor@aim.com>
2019-12-31 13:00:46 -0500

    Update formats.js
    spreadsheet link to correct page

--- config/formats.js
+++ config/formats.js
@@ -613,7 +613,7 @@ exports.Formats = [
 		desc: `A brand new micrometagame from scratch, varied compact metagame existing separately from any tier.`,
   		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/.3652540/">Clean Slate: Micro</a>`,
-			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1GNLvQsM1F6pw1JS7IA6IyrgME1iJ4M0UWLrieGSPQuU/edit?usp=sharing">Spreadsheet of Changes</a>`,
+			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1GNLvQsM1F6pw1JS7IA6IyrgME1iJ4M0UWLrieGSPQuU/edit#gid=1994258282">Spreadsheet of Changes</a>`,
 		      ],
   		ruleset: ['Sleep Clause Mod', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod', 'Team Preview', 'Swagger Clause', 'Baton Pass Clause', 'Dynamax Clause'],
 		mod: 'cleanslatemicro',
