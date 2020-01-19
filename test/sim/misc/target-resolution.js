c5211e96b314f88e89566cb9b22bce36a796efef
scoopapa <pccompositor@aim.com>
2020-01-19 13:31:20 -0600

    copy in from main

--- test/sim/misc/target-resolution.js
+++ test/sim/misc/target-resolution.js
@@ -44,7 +44,7 @@ describe('Target Resolution', function () {
 		});
 
 		it(`should support RedirectTarget event for a fainted foe and type 'any' `, function () {
-			battle = common.createBattle({gameType: 'triples'}, [[
+			battle = common.gen(5).createBattle({gameType: 'triples'}, [[
 				{species: 'Wailord', item: 'laggingtail', ability: 'pressure', moves: ['waterpulse']}, // Water Pulse over Water Gun due to targeting in triples
 				{species: 'Magikarp', ability: 'rattled', moves: ['splash']},
 				{species: 'Magikarp', ability: 'rattled', moves: ['splash']},
@@ -60,7 +60,7 @@ describe('Target Resolution', function () {
 
 			// Do it again with swapped positions
 			battle.destroy();
-			battle = common.createBattle({gameType: 'triples'}, [[
+			battle = common.gen(5).createBattle({gameType: 'triples'}, [[
 				{species: 'Wailord', item: 'laggingtail', ability: 'pressure', moves: ['watergun']},
 				{species: 'Magikarp', ability: 'rattled', moves: ['splash']},
 				{species: 'Magikarp', ability: 'rattled', moves: ['splash']},
@@ -170,6 +170,32 @@ describe('Target Resolution', function () {
 			battle.makeChoices('auto', 'auto'); // Shedinjas faint
 			battle.makeChoices('move watergun -2, pass', 'auto');
 			assert.statStage(redirector, 'spa', 2);
+		});
+
+		it(`should smart-track targets for Stalwart`, function () {
+			battle = common.createBattle({gameType: 'doubles'}, [[
+				{species: 'Duraludon', ability: 'stalwart', moves: ['watergun']},
+				{species: 'Ninjask', ability: 'runaway', moves: ['splash']},
+			], [
+				{species: 'Gastrodon', ability: 'runaway', moves: ['splash']},
+				{species: 'Ninjask', ability: 'runaway', moves: ['allyswitch']},
+			]]);
+
+			battle.makeChoices('move watergun 1, move splash', 'auto');
+			assert.notEqual(battle.p2.active[1].hp, battle.p2.active[1].maxhp);
+		});
+
+		it(`should smart-track targets for Snipe Shot`, function () {
+			battle = common.createBattle({gameType: 'doubles'}, [[
+				{species: 'Duraludon', ability: 'runaway', moves: ['snipeshot']},
+				{species: 'Ninjask', ability: 'runaway', moves: ['splash']},
+			], [
+				{species: 'Gastrodon', ability: 'runaway', moves: ['splash']},
+				{species: 'Ninjask', ability: 'runaway', moves: ['allyswitch']},
+			]]);
+
+			battle.makeChoices('move snipeshot 1, move splash', 'auto');
+			assert.notEqual(battle.p2.active[1].hp, battle.p2.active[1].maxhp);
 		});
 	});
 });
