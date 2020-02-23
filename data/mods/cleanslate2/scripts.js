93d7d63c98851070df02795fd3501486165be440
DatSpookTho <nickguin123@gmail.com>
2020-02-23 17:27:55 -0500

    Add files via upload

---
+++ data/mods/cleanslate2/scripts.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/** @type {ModdedBattleScriptsData} */
+let BattleScripts = {
+	init() {
+	this.modData('Learnsets', 'weezinggalar').learnset.gunkshot = ['7L1'];
+
+	this.modData('Learnsets', 'orbeetle').learnset.overdrive = ['7L1'];
+	this.modData('Learnsets', 'orbeetle').learnset.seismictoss = ['7L1'];
+	this.modData('Learnsets', 'orbeetle').learnset.thunderwave = ['7L1'];
+
+	this.modData('Learnsets', 'stonjourner').learnset.shadowbone = ['7L1'];
+	this.modData('Learnsets', 'stonjourner').learnset.shadowsneak = ['7L1'];
+	this.modData('Learnsets', 'stonjourner').learnset.swordsdance = ['7L1'];
+
+	this.modData('Learnsets', 'cherrim').learnset.fireblast = ['7L1'];
+	this.modData('Learnsets', 'cherrim').learnset.firepunch = ['7L1'];
+	this.modData('Learnsets', 'cherrim').learnset.flamethrower = ['7L1'];
+
+	this.modData('Learnsets', 'zacian').learnset.uturn = ['7L1'];
+
+	this.modData('Learnsets', 'dubwool').learnset.icebeam = ['7L1'];
+	this.modData('Learnsets', 'dubwool').learnset.moonlight = ['7L1'];
+	this.modData('Learnsets', 'dubwool').learnset.sheepiousrend = ['7L1'];
+	this.modData('Learnsets', 'dubwool').learnset.stealthrock = ['7L1'];
+	this.modData('Learnsets', 'dubwool').learnset.teleport = ['7L1'];
+	this.modData('Learnsets', 'dubwool').learnset.thunderbolt = ['7L1'];
+
+	this.modData('Learnsets', 'gumshoos').learnset.bulkup = ['7L1'];
+	this.modData('Learnsets', 'gumshoos').learnset.quickattack = ['7L1'];
+	this.modData('Learnsets', 'gumshoos').learnset.slackoff = ['7L1'];
+
+	this.modData('Learnsets', 'snorlax').learnset.aromatherapy = ['7L1'];
+	this.modData('Learnsets', 'snorlax').learnset.gravapple = ['7L1'];
+	this.modData('Learnsets', 'snorlax').learnset.leechseed = ['7L1'];
+	this.modData('Learnsets', 'snorlax').learnset.slackoff = ['7L1'];
+	this.modData('Learnsets', 'snorlax').learnset.stealthrock = ['7L1'];
+	this.modData('Learnsets', 'snorlax').learnset.woodhammer = ['7L1'];
+
+	this.modData('Learnsets', 'walrein').learnset.anchorshot = ['7L1'];
+	this.modData('Learnsets', 'walrein').learnset.bodypress = ['7L1'];
+	this.modData('Learnsets', 'walrein').learnset.scald = ['7L1'];
+	this.modData('Learnsets', 'walrein').learnset.slackoff = ['7L1'];
+
+	this.modData('Learnsets', 'dhelmise').learnset.scald = ['7L1'];
+	this.modData('Learnsets', 'dhelmise').learnset.spiritshackle = ['7L1'];
+	this.modData('Learnsets', 'dhelmise').learnset.wish = ['7L1'];
+
+	this.modData('Learnsets', 'togekiss').learnset.oblivionwing = ['7L1'];
+	this.modData('Learnsets', 'togekiss').learnset.uturn = ['7L1'];
+
+	this.modData('Learnsets', 'scolipede').learnset.drillrun = ['7L1'];
+	this.modData('Learnsets', 'scolipede').learnset.uturn = ['7L1'];
+
+	this.modData('Learnsets', 'cursola').learnset.healbell = ['7L1'];
+	this.modData('Learnsets', 'cursola').learnset.knockoff = ['7L1'];
+	this.modData('Learnsets', 'cursola').learnset.recover = ['7L1'];
+	}
+
+};
+
+exports.BattleScripts = BattleScripts;
