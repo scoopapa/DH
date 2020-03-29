9520bfd1f08151bbfaca227aa0a653abeb597278
scoopapa <pccompositor@aim.com>
2020-03-28 19:51:57 -0500

    CS2 Slate 7

--- data/mods/cleanslate2/formats-data.js
+++ data/mods/cleanslate2/formats-data.js
@@ -4,4 +4,16 @@ exports.BattleFormatsData = {
 	cherrimsunshine: {
 		tier: "CS2",
 	},
+	unownp: {
+		tier: "CS2",
+	},
+	unowns: {
+		tier: "CS2",
+	},
+	unownm: {
+		tier: "CS2",
+	},
+	shayminsky: {
+		requiredItems: ["Flying Memory"],
+	},
 };
