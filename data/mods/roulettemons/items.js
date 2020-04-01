e5039ae4e7cbe80dcc9a04c975d658c49f38fccb
scoopapa <pccompositor@aim.com>
2020-04-01 00:06:09 -0500

    Create items.js

---
+++ data/mods/roulettemons/items.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.BattleItems = {
+	"chillytite": {
+		id: "chillytite",
+		name: "Chillytite",
+		spritenum: 594,
+		megaStone: "Chillyte-Mega",
+		megaEvolves: "Chillyte",
+		itemUser: ["Chillyte"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseTemplate.baseSpecies) return false;
+			return true;
+		},
+		num: 673,
+		gen: 8,
+		desc: "If held by a Lucario, this item allows it to Mega Evolve in battle.",
+	},
+};
