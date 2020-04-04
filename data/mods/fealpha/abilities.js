6e3029b4cd7098857c00ee8d0603380238d950b8
charizard8888 <omkumr082001@outlook.com>
2020-04-04 15:51:15 +0530

    Update abilities.js

--- data/mods/fealpha/abilities.js
+++ data/mods/fealpha/abilities.js
@@ -90,7 +90,77 @@ let BattleAbilities = {
 		rating: 2,
 		num: 251,
 	},
-	
+		"terraform": {
+		shortDesc: " This Pokemon is immune to Ground-type attacks, and raises its highest non-HP stat when hit by one or when it gets a KO.",
+		id: "terraform",
+		name: "Terraform",
+		onTryHit(target, source, move, effect) {
+			if (target !== source && move.type === 'Ground') {
+				if (effect && effect.effectType === 'Move') {
+				let statName = 'atk';
+				let bestStat = 0;
+				/** @type {StatNameExceptHP} */
+				let s;
+				for (s in source.storedStats) {
+					if (source.storedStats[s] > bestStat) {
+						statName = s;
+						bestStat = source.storedStats[s];
+					}
+				}
+				this.boost({[statName]: 1}, source);
+			}
+				if (!this.heal(target.maxhp / 4)) {
+					this.add('-immune', target, '[msg]', '[from] ability: Terraform');
+				}
+				return null;
+			}
+		},
+		rating: 3.5,
+		num: 26,
+	},
+		"thunderclap": {
+		shortDesc: "On switch-in, this Pokemon lowers the Attack of adjacent opponents by 1 stage and restores 1/4 of its maximum HP, rounded down. This Pokemon is immune to Electric-type moves.",
+		onStart(pokemon) {
+			let activated = false;
+			for (const target of pokemon.side.foe.active) {
+				if (!target || !this.isAdjacent(target, pokemon)) continue;
+				if (!activated) {
+					this.add('-ability', pokemon, 'Intimidate', 'boost');
+					activated = true;
+				}
+				if (target.volatiles['substitute']) {
+					this.add('-immune', target);
+				} else if (target.hasAbility(['Inner Focus', 'Oblivious', 'Own Tempo', 'Scrappy'])) {
+					this.add('-immune', target, `[from] ability: ${target.getAbility().name}`);
+				} else {
+					this.boost({atk: -1}, target, pokemon, null, true);
+					pokemon.heal(pokemon.baseMaxhp / 4);
+				}
+			}
+		},
+		id: "thunderclap",
+		name: "Thunderclap",
+		rating: 3.5,
+		num: 22,
+	},
+		"passionstar": {
+		shortDesc: "This Pokemon and its allies' moves have their Attack and accuracy multiplied by 1.1.",
+		onAllyModifyMove(move) {
+			if (typeof move.accuracy === 'number') {
+				move.accuracy *= 1.1;
+			}
+		},
+		onModifyAtk(atk, attacker, defender, move) {
+				return this.chainModify(1.1);
+		},
+		onAllyModifyAtk(atk, attacker, defender, move) {
+				return this.chainModify(1.1);
+		},
+		id: "passionstar",
+		name: "Passion Star",
+		rating: 2.5,
+		num: 162,
+	},
 };
 
 exports.BattleAbilities = BattleAbilities;
