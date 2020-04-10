a589cb56c52baa6c170c04de88f5a9fae8ad65ba
scoopapa <pccompositor@aim.com>
2020-04-09 19:16:19 -0500

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -661,7 +661,7 @@ exports.Formats = [
 		},
 	},
 	{
-		name: "[Gen 8] Roulettemons",
+		name: "[Gen 8 Pet Mod] Roulettemons",
 		desc: [`A competitive metagame made up of brand new Pokémon that are randomly generated on every competitive aspect (typing, moves, stats, abilities).`],
 		threads: [`&bullet; <a href=https://www.smogon.com/forums/threads/.3649106/>Roulettemons</a>`,
 		     ], 
