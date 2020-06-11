24c825361cb3b3208437562d51177037b46f1d5b
pika-xreme <53130403+pika-xreme@users.noreply.github.com>
2020-06-11 15:48:00 -0400

    Add RRIB

--- config/formats.js
+++ config/formats.js
@@ -956,6 +956,18 @@ exports.Formats = [
 				pokemon.addVolatile('twisted');
 		}
 	},
+	{
+		name: "[Gen 1] Rose Red / Iris Blue",
+		desc: `A balance mod for Gen 1 that aims to make every fully-evolved Pokémon a viable pick.`,
+		threads: [
+			`<a href="https://www.smogon.com/forums/threads/gen-1-rose-red-iris-blue.3652237/">Rose Red / Iris Blue</a>`,
+		],
+		mod: 'roseredirisblue',
+		ruleset: ['Standard'],
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.template.types.join('/'), '[silent]');
+		},
+	},
 	// Old Pet Mods ///////////////////////////////////////////////////////////////////
 	{
 		section: "Old Pet Mods",
