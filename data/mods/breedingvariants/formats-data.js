7e426e95dd5ba1f775058b3ea49e4c81da5f93ed
scoopapa <pccompositor@aim.com>
2020-06-16 08:45:24 -0500

    breeding variants

--- data/mods/breedingvariants/formats-data.js
+++ data/mods/breedingvariants/formats-data.js
@@ -5,34 +5,34 @@ exports.BattleFormatsData = {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	electivirekungfu: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
+	// electivirekungfu: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
 	flygonbuzz: {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	banetteblademaster: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
-	banettemegablademaster: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
-	kingdracamo: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
+	// banetteblademaster: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
+	// banettemegablademaster: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
+	// kingdracamo: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
 	vikavoltmigale: {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	typhlosioncinder: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
+	// typhlosioncinder: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
 	flareonocean: {
 		tier: "OU",
 		doublesTier: "DOU",
@@ -41,10 +41,10 @@ exports.BattleFormatsData = {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	exploudmeow: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
+	// exploudmeow: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
 	butterfreearmor: {
 		tier: "OU",
 		doublesTier: "DOU",
@@ -61,30 +61,30 @@ exports.BattleFormatsData = {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	delphoxlazy: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
-	lurantisnut: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
+	// delphoxlazy: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
+	// lurantisnut: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
 	sylveonprotector: {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	skarmoryprimordial: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
-	infernapebugout: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
-	breloomluau: {
-		tier: "OU",
-		doublesTier: "DOU",
-	},
+	// skarmoryprimordial: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
+	// infernapebugout: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
+	// breloomluau: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
 	leafeoncutlass: {
 		tier: "OU",
 		doublesTier: "DOU",
@@ -93,11 +93,55 @@ exports.BattleFormatsData = {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	dodriomystical: {
+	// dodriomystical: {
+		// tier: "OU",
+		// doublesTier: "DOU",
+	// },
+	mantinespiny: {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
-	mantinespiny: {
+	rapidashgalarmeow: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mrrimespoon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	thievulbananas: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	stonjournercastle: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	copperajahforge: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	grapploctray: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	appletunburned: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	vespiquenterra: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	cursolashock: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	machamplucha: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	roseradescarfed: {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
