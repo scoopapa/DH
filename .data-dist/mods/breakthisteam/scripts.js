e6d979e1d27df9898f47e30bbba1564a9859305f
Landorus-Tea <83421264+Landorus-Tea@users.noreply.github.com>
2021-05-09 17:59:45 -0400

    aa
    ff

--- .data-dist/mods/breakthisteam/scripts.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); const Scripts = {
-	battle: {
-		lostItemQueue: Array(),
-	},
-}; exports.Scripts = Scripts;
--- .data-dist/mods/m4asandbox/formats-data.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); const FormatsData = {
-	dragonitemega: {
-		tier: "Sandbox",
-	},
-	porygonzmega: {
-		tier: "God",
-	},
-}; exports.FormatsData = FormatsData;
